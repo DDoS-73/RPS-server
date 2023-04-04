a657f16881d9dff6edc9fb022e841520452c9604
”Vlad” <vp.9433@gmail.com>
2023-04-04 17:20:13 +0300

    feat: rewrite logs message

--- server.js
+++ server.js
@@ -11,44 +11,25 @@ const io = new Server(httpServer, {
 	transports: ['websocket', 'polling', 'flashsocket'],
 });
 
-const store = {};
-
 io.on("connection", (socket) => {
-	console.log('new socket connected', socket.id);
-	console.log('socket amount', io.sockets.sockets.size);
+	console.log('New socket connected', socket.id);
+	console.log('Socket amount', io.sockets.sockets.size);
 
 	socket.on("join", (roomID) => {
 		socket.join(roomID);
-		console.log('roomID', roomID);
+		console.log(`Socket ${socket.id} connected to room ${roomID}`);
 
 		const size = io.sockets.adapter.rooms.get(roomID).size;
-		console.log('roomSize', size);
-		if(size === 2) {
-			store[roomID] = new Map();
-		}
+		console.log(`Room ${roomID} size is ${size}`);
 
 		io.in(roomID).emit('game_start', { started: size >= 2 });
 	})
 
 	socket.on('move', (sign) => {
-		console.log(sign);
 		const roomID = Array.from(socket.rooms)[1];
-		// store[roomID].set(socket, {
-		// 	sign,
-		// 	score: store[roomID].get(socket)?.score ?? 0,
-		// })
-		// if (store[roomID].size === 2) {
-		// 	const arr = Array.from(store[roomID].values());
-		// 	const res = playRound(arr[0].sign, arr[1].sign);
-		// 	if(!res) {
-		// 		store[roomID].get(socket).score += 1;
-		// 		socket.emit('opponent_move', {sign});
-		// 	} else {
-		//
-		// 	}
-		// }
 		socket.to(roomID).emit('opponent_move', sign);
-	})
+	});
+
 });
 
 io.listen(8000);
